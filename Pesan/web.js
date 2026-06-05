05bf8234a938048c05987d117ee6e8c79b4d92a7
satriopambudi12 <vinz.tyo@gmail.com>
2026-06-06 02:52:12 +0700

    Update Data

---
+++ Pesan/web.js
@@ -0,0 +1,50 @@
+// DATA DUMMY
+
+const projects = [
+    {
+        id: 1,
+        name: 'Subagyo',
+        description: 'Belajar Java Script'
+    },
+    {
+        id: 2,
+        name: 'Cahyadi',
+        description: 'Belajar di Dumbways'
+    }
+];
+
+function renderProjects() {
+    const container = document.getElementById('projectList')
+
+    let projectsHTML = '';
+
+    for (let i = 0; i < projects.length; i++) {
+        const project = projects[i];
+
+        projectsHTML += `<div class="card">
+        <h4>${project.name}</h4>
+        <p>${project.description}</p>
+      </div>`
+    }
+    container.innerHTML = projectsHTML
+}
+renderProjects();
+
+// FORM SUBMIT
+
+const from = document.getElementById('projectFrom')
+
+from.addEventListener('submit', function(event){
+    event.preventDefault();
+
+    const name = document.getElementById('projectName').value;
+    const description = document.getElementById('description').value;
+
+    const newProject = {
+        id: projects.length + 1,
+        name: name,
+        description: description,
+    };
+    projects.push(newProject);
+    renderProjects();
+})
--- index.js
+++ index.js
@@ -1,50 +1,58 @@
 // DATA DUMMY
-
-const projects = [
+const dataLokal = localStorage.getItem('myProjects');
+const projects = dataLokal ? JSON.parse(dataLokal) : [
     {
         id: 1,
-        name: 'Subagyo',
-        description: 'Belajar Java Script'
+        name: 'Makan Siang',
+        description: 50000
     },
-    {
-        id: 2,
-        name: 'Cahyadi',
-        description: 'Belajar di Dumbways'
-    }
 ];
 
+function formatRupiah(angka) {
+    return new Intl.NumberFormat('id-ID', {
+        style: 'currency',
+        currency: 'IDR',
+        minimumFractionDigits: 0
+    }).format(angka);
+}
+
 function renderProjects() {
-    const container = document.getElementById('projectList')
+    const container = document.getElementById('projectHasil')
 
     let projectsHTML = '';
 
     for (let i = 0; i < projects.length; i++) {
         const project = projects[i];
-
+    
         projectsHTML += `<div class="card">
-        <h3>${project.name}</h3>
-        <p>${project.description}</p>
+        <h4>${project.name}</h4>
+        <p>${formatRupiah(project.description)}</p>
       </div>`
     }
     container.innerHTML = projectsHTML
 }
+
 renderProjects();
 
-// FORM SUBMIT
-
-const from = document.getElementById('projectFrom')
+const from = document.getElementById('projectHitung')
 
 from.addEventListener('submit', function(event){
     event.preventDefault();
 
-    const name = document.getElementById('projectName').value;
-    const description = document.getElementById('description').value;
+    const name = document.getElementById('projectPengeluaran').value;
+    const description = Number(document.getElementById('projectHarga').value);
 
     const newProject = {
         id: projects.length + 1,
         name: name,
         description: description,
     };
+    
     projects.push(newProject);
+    
+    localStorage.setItem('myProjects', JSON.stringify(projects));
+    
     renderProjects();
+    
+    event.target.reset(); 
 })
